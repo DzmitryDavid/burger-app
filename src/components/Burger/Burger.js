6455638bc2e6aab438ffcef965458c254fb0217e
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-07-01 02:26:40 +0300

    add calculating ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,23 @@ import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 import './Burger.scss';
 
 const Burger = (props) => {
+
   const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
     return [...Array(props.ingredients[igKey])].map((_, ind) => {
       return <BurgerIngredient key={igKey + ind} type={igKey}/>
     })
-  })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, [])
+  
+    
+  
   console.log(transformedIngredients);
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top"/>
-      {transformedIngredients}
-      <BurgerIngredient type="bread-bottom"/>
+      {(transformedIngredients.length === 0) ? "Please start adding ingredients" : transformedIngredients }
+      <BurgerIngredient type="bread-bottom"/> 
     </div>
   )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,8 +5,8 @@ class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 1,
-      cheese: 2,
-      meat: 2,
+      cheese: 1,
+      meat: 1,
       bacon: 1
     }
 
