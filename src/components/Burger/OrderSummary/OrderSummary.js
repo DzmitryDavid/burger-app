8f798925be4bdf76e1e654b2978b7ac820336717
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-07-02 00:19:36 +0300

    create order modal window

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+  .map(ingredient => <li><span> {ingredient} : {props.ingredients[ingredient]} </span>  </li>)
+  
+  return (
+    <>
+    <h3>Your order</h3>
+    <p>Your burger with following ingredients :</p>
+    <ul>
+      {ingredientSummary}
+    </ul>
+    <p>Continue to Checkout?</p>
+    </>
+  )
+}
+
+export default OrderSummary
