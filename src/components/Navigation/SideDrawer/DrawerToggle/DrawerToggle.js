9d2d2f3f8bfbba3e0a8fda222f0522139a502db2
Dzmitry <Dzmitrydavidovich89@gmail.com>
2021-07-04 22:45:05 +0300

    refactor: update structure

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import './DrawerToggle.scss';
 
 const DrawerToggle = (props) => (
-  <div onClick={props.clicked}>Menu</div>
+  <div
+    className="DrawerToggle"
+    onClick={props.clicked}>
+
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 )
 
 export default DrawerToggle
