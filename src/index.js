3992107763c2f572cf3dfc80819e3c5a98487743
Dzmitry <Dzmitrydavidovich89@gmail.com>
2021-09-20 11:53:53 +0300

    add redux store to project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import reducer from './store/reducer';
+import App from './App';
+import './index.scss';
 
+const store = createStore(reducer)
 
 
 ReactDOM.render(
   
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-  </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionsTypes from './actions';
+
+const initState = {
+  ingredients: null,
+  totalPrice: 4
+}
+
+const reducer = (state=initState, action) => {
+  
+}
+
+export default reducer;
